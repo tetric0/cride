400b77766299912c57d0ed09e0d835c482df1d81
victor.munoz <victor.munoz@upc.edu>
2022-03-14 14:26:20 +0100

    Video 14 del 'Curso Avanzado de Django 2019' acabado: Creada y registrada la aplicacion 'Circles' con su modelo asociado.

---
+++ cride/circles/apps.py
@@ -0,0 +1,11 @@
+"""Circles app."""
+
+# Django
+from django.apps import AppConfig
+
+
+class CirclesAppConfig(AppConfig):
+    """Circles App Config."""
+
+    name = 'cride.circles'
+    verbose_name = 'Circles'
