87dd43da622854ea66ef15e0fdfd091a2a5d38ea
victor.munoz <victor.munoz@upc.edu>
2022-03-15 10:38:29 +0100

    Video 16 del'Curso Avanzado de Django 2019': Terminada la creacion de una API para el listado de Circles, sin Django Rest Framework.

---
+++ cride/circles/urls.py
@@ -0,0 +1,11 @@
+"""Circles URLs Module."""
+
+# Django
+from django.urls import path
+
+# Views
+from cride.circles.views import list_circles
+
+urlpatterns = [
+    path('circles/', list_circles)
+]
